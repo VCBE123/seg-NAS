763de8a83792592817ecd1cdf4a9375fd7d7ebf5
vcbe123 <945193029@qq.com>
2019-10-10 15:48:30 +0800

    fix typo

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 "utils with averagmeter,bestmeter,dice"
-from .metric import AverageMeter, BestMeter, get_dice_follicle, get_dice_overay
+from .metric import AverageMeter, BestMeter, get_dice_follicle, get_dice_ovary
 from ._utils import create_exp_dir, notice, count_parameters, save_checkpoint
