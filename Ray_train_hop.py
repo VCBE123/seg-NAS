8c378f3d3b309c51285eeb0319970474150df428
vcbe123 <945193029@qq.com>
2019-11-26 17:52:51 +0800

    add Dense

--- Ray_train_hop.py
+++ Ray_train_hop.py
@@ -12,7 +12,7 @@ import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import numpy as np
-from nas import NASRayNetEval, WeightDiceLoss, ray2, ray3, MultipleOptimizer
+from nas import NASRayNetEval_v0, NASRayNetEval_v0_dense, WeightDiceLoss, ray2, ray3, MultipleOptimizer
 from dataloader import get_follicle
 from utils import AverageMeter, create_exp_dir, count_parameters, notice, save_checkpoint, get_dice_follicle, get_dice_ovary
 
@@ -29,7 +29,7 @@ def get_parser():
     parser = argparse.ArgumentParser(description='train unet')
     parser.add_argument('--workers', type=int, default=8)
     parser.add_argument('--batch_size', type=int, default=16)
-    parser.add_argument('--learning_rate', type=float, default=5e-4)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight_decay', type=float, default=1e-4)
     parser.add_argument('--report', type=int, default=100)
@@ -51,10 +51,12 @@ torch.manual_seed(ARGS.seed)
 torch.cuda.manual_seed(ARGS.seed)
 
 
-def train_follicle(config):
+def train_follicle_dense(config):
     "train follicle"
-
-    model = NASRayNetEval(genotype=ray2)
+    if config["Dense"]:
+        model = NASRayNetEval_v0_dense(genotype=ray2)
+    else:
+        model = NASRayNetEval_v0(genotype=ray2)
     model = nn.DataParallel(model)
     model = model.cuda()
     encoder_parameters = []
@@ -65,44 +67,31 @@ def train_follicle(config):
         else:
             decoder_parameters.append(parameter)
 
-    optimizer_encoder = torch.optim.SGD(
-        encoder_parameters, config["learning_rate"], momentum=ARGS.momentum, weight_decay=config["weight_decay"])
-    optimizer_decoder = torch.optim.SGD(
-        decoder_parameters, config["learning_rate"]*config["times"], weight_decay=config["weight_decay"], momentum=ARGS.momentum)
-
+    optimizer_encoder = torch.optim.Adam( encoder_parameters, config["learning_rate"], weight_decay=config["weight_decay"]) 
+    optimizer_decoder = torch.optim.Adam( decoder_parameters, config["learning_rate"]*config["times"], weight_decay=config["weight_decay"])
     multop = MultipleOptimizer(optimizer_decoder, optimizer_encoder)
-
-    lr_scheduler_encoder = torch.optim.lr_scheduler.StepLR(
-        optimizer_encoder, step_size=config["step"], gamma=0.1)
-    lr_scheduler_decoder = torch.optim.lr_scheduler.StepLR(
-        optimizer_decoder, step_size=config["step"], gamma=0.1)
     criterion = WeightDiceLoss().cuda()
     train_loader, val_loader = get_follicle(
         config["batch_size"], 8, train_aug=True)
     best_dice_follicle = 0
     for epoch in range(ARGS.epochs):
-        current_lr_encoder = optimizer_encoder.param_groups[0]['lr']
-        current_lr_decoder = optimizer_decoder.param_groups[0]['lr']
         train_loss = train(train_loader, model, criterion,
                            multop, accumulate=config["accumulate"])
-        lr_scheduler_encoder.step()
-        lr_scheduler_decoder.step()
         valid_dice_follicle, valid_dice_ovary, valid_loss = infer(
             val_loader, model, criterion)
         is_best = False
         if valid_dice_follicle > best_dice_follicle:
             best_dice_follicle = valid_dice_follicle
             is_best = True
-            if valid_dice_follicle>0.89:
+            if valid_dice_follicle > 0.89:
                 try:
                     notice('validation-search',
-                        message="epoch:{} best_dice:{}".format(epoch, best_dice_follicle))
+                           message="epoch:{} best_dice:{}".format(epoch, best_dice_follicle))
                 finally:
                     pass
         save_checkpoint({'epoch': epoch+1, 'state_dict': model.state_dict(),
                          'best_dice_follicle': best_dice_follicle, 'optimizer': optimizer_decoder.state_dict()}, is_best, "")
-        track.log(lr_encoder=current_lr_encoder, lr_decoder=current_lr_decoder, train_loss=train_loss, valid_loss=valid_loss,
-                  valid_dice_follicle=valid_dice_follicle, valid_dice_ovary=valid_dice_ovary, best_dice_follicle=best_dice_follicle)
+        track.log( train_loss=train_loss, valid_loss=valid_loss, valid_dice_follicle=valid_dice_follicle, valid_dice_ovary=valid_dice_ovary, best_dice_follicle=best_dice_follicle)
 
 
 def train(train_loader, model, criterion, optimizer, accumulate):
@@ -145,19 +134,17 @@ def infer(valid_loader, model, criterion):
 
 
 if __name__ == '__main__':
-    os.environ["CUDA_VISIBLE_DEVICES"]='0,1,2,3,4,5'
+    os.environ["CUDA_VISIBLE_DEVICES"] = '1,2,3,4,5,6'
     ray.init(num_gpus=6, ignore_reinit_error=True)
-    sched = ASHAScheduler(metric="best_dice_follicle", mode="max")
-    search_space = {"learning_rate": tune.choice([1e-3, 5e-4, 1e-4, 5e-3]),
-                    "step": tune.choice([5, 7, 9, 11, 13]),
-                    "weight_decay": tune.choice([1e-4, 1e-5, 1e-3]),
-                    "accumulate": tune.choice([1, 2, 4, 6, 8]),
-                    "batch_size": tune.choice([8, 12, 16]),
-                    "times": tune.choice([1, 2, 3, 4, 5])}
-    analysis = tune.run(
-        train_follicle,
-        scheduler=sched,
-        num_samples=100,
+    # sched = ASHAScheduler(metric="best_dice_follicle", mode="max")
+    search_space = {
+        "Dense":tune.choice([True,False]),
+        "learning_rate": tune.choice([ 1e-4 ]),
+                    "weight_decay": tune.choice([ 1e-5]),
+                    "accumulate": tune.choice([  6]),
+                    "batch_size": tune.choice([16]),
+                    "times": tune.choice([ 2])}
+    analysis = tune.run( train_follicle, num_samples=100,
         stop={"best_dice_follicle": 90.0},
         resources_per_trial={"cpu": 8, "gpu": 3}, config=search_space)
     print("Best config:", analysis.get_best_config(metric="best_dice_follicle"))
